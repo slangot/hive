a1af75c5322c0d988f1b8ffe5219ada6332b7660
Shawn <shawn17@live.fr>
2024-04-22 16:00:24 +0200

    🚚 put images in public directory

--- app/page.js
+++ app/page.js
@@ -1,6 +1,24 @@
 "use client"
 import React, { useEffect, useRef, useState } from "react";
 
+// imports
+import bee1 from "../public/bee-1-sm-2.png";
+import bee2 from "../public/bee-2.png";
+import bee3 from "../public/bee-3.png";
+import bg1Sm2 from "../public/bg-1-sm-2.jpg";
+import boardSkills1 from "../public/board-skills-1.png";
+import grassFlower1 from "../public/grass-flower-1.png";
+import hiveAbbeal from "../public/hive-2-abbeal.png";
+import hiveBtn1 from "../public/hive-btn-1-min.png";
+import hiveLayout from "../public/hive-layout-2.png";
+import logosX3_1 from "../public/logos-x3-1.png";
+import logosX3_2 from "../public/logos-x3-2.png";
+import logosX3_3 from "../public/logos-x3-3.png";
+import panel1 from "../public/panel-1.png";
+import panel2 from "../public/panel-2.png";
+import rope from "../public/rope.png";
+
+
 // next
 import Image from "next/image";
 
@@ -99,7 +117,8 @@ export default function Home() {
         moveBee(-1, windowWidth - (scrollY - windowWidth), beeTopPosition);
 
         // Animated element effects
-        moveElement("/static/images/logos-x3-1.png", -1, windowWidth - (scrollY - windowWidth) + dynamicElementLeftPosition, dynamicElementTopPosition);
+        // moveElement("/static/images/logos-x3-1.png", -1, windowWidth - (scrollY - windowWidth) + dynamicElementLeftPosition, dynamicElementTopPosition);
+        moveElement(logosX3_1, -1, windowWidth - (scrollY - windowWidth) + dynamicElementLeftPosition, dynamicElementTopPosition);
       }
 
       /////// SECOND WAY Left -> Right
@@ -126,8 +145,10 @@ export default function Home() {
         moveBee(-1, windowWidth - (scrollY - windowWidth * 3), 120);
 
         // Animated element effects
-        showElement(dynamicElement, windowWidth - (scrollY - windowWidth * 3) + dynamicElementLeftPosition, 220, "/static/images/logos-x3-2.png", 80);
-        moveElement("/static/images/logos-x3-2.png", -1, windowWidth - (scrollY - windowWidth * 3) + dynamicElementLeftPosition, dynamicElementTopPosition);
+        // showElement(dynamicElement, windowWidth - (scrollY - windowWidth * 3) + dynamicElementLeftPosition, 220, "/static/images/logos-x3-2.png", 80);
+        // moveElement("/static/images/logos-x3-2.png", -1, windowWidth - (scrollY - windowWidth * 3) + dynamicElementLeftPosition, dynamicElementTopPosition);
+        showElement(dynamicElement, windowWidth - (scrollY - windowWidth * 3) + dynamicElementLeftPosition, 220, logosX3_2, 80);
+        moveElement(logosX3_2, -1, windowWidth - (scrollY - windowWidth * 3) + dynamicElementLeftPosition, dynamicElementTopPosition);
       }
 
       /////// THIRD WAY Left -> Right
@@ -155,8 +176,10 @@ export default function Home() {
         moveBee(-1, windowWidth - (scrollY - windowWidth * 5), 120);
 
         // Animated element effects
-        showElement(dynamicElement, windowWidth - (scrollY - windowWidth * 5) + dynamicElementLeftPosition, 220, "/static/images/logos-x3-3.png", 80);
-        moveElement("/static/images/logos-x3-3.png", -1, windowWidth - (scrollY - windowWidth * 5) + dynamicElementLeftPosition, dynamicElementTopPosition);
+        // showElement(dynamicElement, windowWidth - (scrollY - windowWidth * 5) + dynamicElementLeftPosition, 220, "/static/images/logos-x3-3.png", 80);
+        // moveElement("/static/images/logos-x3-3.png", -1, windowWidth - (scrollY - windowWidth * 5) + dynamicElementLeftPosition, dynamicElementTopPosition);
+        showElement(dynamicElement, windowWidth - (scrollY - windowWidth * 5) + dynamicElementLeftPosition, 220, logosX3_3, 80);
+        moveElement(logosX3_3, -1, windowWidth - (scrollY - windowWidth * 5) + dynamicElementLeftPosition, dynamicElementTopPosition);
       }
 
       /////// FOURTH WAY Left -> Right
@@ -182,8 +205,10 @@ export default function Home() {
         moveBee(-1, windowWidth - (scrollY - windowWidth * 7), 120);
 
         // Animated element effects
-        showElement(dynamicElement, windowWidth - (scrollY - windowWidth * 7), 220, "/static/images/board-skills-1.png", 200);
-        moveElement("/static/images/board-skills-1.png", -1, windowWidth - (scrollY - windowWidth * 7) - 50, dynamicElementTopPosition);
+        // showElement(dynamicElement, windowWidth - (scrollY - windowWidth * 7), 220, "/static/images/board-skills-1.png", 200);
+        // moveElement("/static/images/board-skills-1.png", -1, windowWidth - (scrollY - windowWidth * 7) - 50, dynamicElementTopPosition);
+        showElement(dynamicElement, windowWidth - (scrollY - windowWidth * 7), 220, boardSkills1, 200);
+        moveElement(boardSkills1, -1, windowWidth - (scrollY - windowWidth * 7) - 50, dynamicElementTopPosition);
       }
 
       /////// FIFTH WAY Left -> Right
@@ -263,7 +288,8 @@ export default function Home() {
         </div>
       </div>}
       {(initWindow.width && initWindow.height) && <>
-        <div className="flex items-center justify-center relative w-full" style={{ backgroundImage: "url(/static/images/bg-1-sm-2.jpg)", backgroundSize: "cover", backgroundAttachment: "fixed", height: "100%" }}>
+        {/* <div className="flex items-center justify-center relative w-full" style={{ backgroundImage: "url(/static/images/bg-1-sm-2.jpg)", backgroundSize: "cover", backgroundAttachment: "fixed", height: "100%" }}> */}
+        <div className="flex items-center justify-center relative w-full" style={{ backgroundImage: `url(/bg-1-sm-2.jpg)`, backgroundSize: "cover", backgroundAttachment: "fixed", height: "100%" }}>
 
           {/**** Texts */}
           <h1 className="title__custom" style={{ top: `${initWindow.height / 2}px`, zIndex: 70 }}>
@@ -292,59 +318,65 @@ export default function Home() {
           {/**** Hive */}
           <div className="relative">
 
-            <img
+            <Image
               className="fixed z-20 top-10 -left-[60px] scale-x-[-1]"
-              src="/static/images/hive-2-abbeal.png"
+              // src="/static/images/hive-2-abbeal.png"
+              src={hiveAbbeal}
               alt="hive with logo"
               width={300}
               height={300}
             />
 
-            <img
+            <Image
               className="fixed z-40 top-10 -left-[60px] scale-x-[-1]"
-              src="/static/images/hive-layout-2.png"
+              // src="/static/images/hive-layout-2.png"
+              src={hiveLayout}
               alt="hive"
               width={300}
               height={300}
             />
 
-            <img className="fixed z-10 left-[100px] top-[250px]"
+            <Image className="fixed z-10 left-[100px] top-[250px]"
               width={100}
               height={100}
-              src="/static/images/rope.png"
+              // src="/static/images/rope.png"
+              src={rope}
               alt="rope"
             />
 
-            <img className="fixed z-40 left-[170px] top-[50px] scale-x-[-1] hidden"
+            <Image className="fixed z-40 left-[170px] top-[50px] scale-x-[-1] hidden"
               ref={bee2Img}
               width={100}
               height={100}
-              src="/static/images/bee-2.png"
+              // src="/static/images/bee-2.png"
+              src={bee2}
               alt="bee 2"
             />
 
-            <img className="fixed z-40 left-[0px] top-[220px] hidden"
+            <Image className="fixed z-40 left-[0px] top-[220px] hidden"
               ref={bee3Img}
               width={100}
               height={100}
-              src="/static/images/bee-3.png"
+              // src="/static/images/bee-3.png"
+              src={bee3}
               alt="bee 3"
             />
 
           </div>
 
           {/**** Dynmaic Bee */}
-          <img className="fixed z-[60] top-[130px] object-contain"
+          <Image className="fixed z-[60] top-[130px] object-contain"
             ref={beeImg}
             width={100}
             height={100}
-            src="/static/images/bee-1-sm-2.png"
+            // src="/static/images/bee-1-sm-2.png"
+            src={bee1}
             alt="bee"
             id="bee-img"
           />
 
           {/**** Animated Element */}
-          <img className="fixed z-50 hidden"
+          <Image className="fixed z-50 hidden"
             ref={dynamicElement}
             src=""
             width={80}
@@ -354,56 +386,62 @@ export default function Home() {
 
           {/**** Static logos in the hive */}
           {/* First x3 static logos ; React, Next.js, TypeScript */}
-          <img className="fixed hidden z-30"
+          <Image className="fixed hidden z-30"
             ref={staticLogos1}
-            src="/static/images/logos-x3-1.png"
+            // src="/static/images/logos-x3-1.png"
+            src={logosX3_1}
             width={60}
             height={60}
             alt="react next ts logo"
           />
 
           {/* Second x3 static logos ; Node.js, MySQL, TailWind */}
-          <img className="fixed hidden z-[29]"
+          <Image className="fixed hidden z-[29]"
             ref={staticLogos2}
-            src="/static/images/logos-x3-2.png"
+            // src="/static/images/logos-x3-2.png"
+            src={logosX3_2}
             width={60}
             height={60}
             alt="node tailwind mysql logo"
           />
 
           {/* Third x3 static logos ; Git, GitHub, BitBucket */}
-          <img className="fixed hidden z-[28]"
+          <Image className="fixed hidden z-[28]"
             ref={staticLogos3}
-            src="/static/images/logos-x3-3.png"
+            // src="/static/images/logos-x3-3.png"
+            src={logosX3_3}
             width={60}
             height={60}
             alt="git github bitbucket logo"
           />
 
           {/* Wood suspended board with x3 static skills ; Agile method, clean code, Jira and Notion */}
-          <img className="fixed hidden z-[40]"
+          <Image className="fixed hidden z-[40]"
             ref={staticBoard1}
-            src='/static/images/board-skills-1.png'
+            // src='/static/images/board-skills-1.png'
+            src={boardSkills1}
             width={200}
             height={0}
             alt='Agile method, clean code, Jira and Notion'
           />
 
           {/* First panel with x3 static skills ;  "Adaptation, bienveillance, volontaire" */}
-          <img className="fixed hidden z-40"
+          <Image className="fixed hidden z-40"
             style={{ width: `${(initWindow.width / 4) >= 200 ? 200 : initWindow.width / 4}px` }}
             ref={staticPanel1}
-            src='/static/images/panel-1.png'
+            // src='/static/images/panel-1.png'
+            src={panel1}
             width={200}
             height={0}
             alt='Adaptation, bienveillance, volontaire'
           />
 
           {/* Second panel with x3 static skills ;  "Motivé, joie de vivre, curieux" */}
-          <img className="fixed hidden z-40"
+          <Image className="fixed hidden z-40"
             style={{ width: `${(initWindow.width / 4) >= 200 ? 200 : initWindow.width / 4}px` }}
             ref={staticPanel2}
-            src='/static/images/panel-2.png'
+            // src='/static/images/panel-2.png'
+            src={panel2}
             width={200}
             height={0}
             alt='Motivé, joie de vivre, curieux'
@@ -412,14 +450,16 @@ export default function Home() {
           {/* Contact button */}
           <div className="flex items-center justify-center w-full h-full">
             <a href='mailto:langot.sylvain.contact@gmail.com'>
-              <button className="fixed hidden z-50 px-3 bg-white font-bold text-orange-500 bg-transparent w-[120px] h-[120px] rounded-lg" style={{ backgroundColor: 'transparent', backgroundImage: 'url(/static/images/hive-btn-1-min.png)', backgroundSize: 'cover', animation: 'beehiveAnimation 5s linear infinite' }} ref={buttonRef}>Me contacter</button>
+              {/* <button className="fixed hidden z-50 px-3 bg-white font-bold text-orange-500 bg-transparent w-[120px] h-[120px] rounded-lg" style={{ backgroundColor: 'transparent', backgroundImage: 'url(/static/images/hive-btn-1-min.png)', backgroundSize: 'cover', animation: 'beehiveAnimation 5s linear infinite' }} ref={buttonRef}>Me contacter</button> */}
+              <button className="fixed hidden z-50 px-3 bg-white font-bold text-orange-500 bg-transparent w-[120px] h-[120px] rounded-lg" style={{ backgroundColor: 'transparent', backgroundImage: `url(/hive-btn-1-min.png)`, backgroundSize: 'cover', animation: 'beehiveAnimation 5s linear infinite' }} ref={buttonRef}>Me contacter</button>
             </a>
           </div>
 
           {/**** Grass */}
           <div
             className="fixed z-30 bottom-0 w-full h-52 bg-cover bg-repeat-x"
-            style={{ backgroundImage: `url('/static/images/grass-flower-1.png')` }}
+            // style={{ backgroundImage: `url('/static/images/grass-flower-1.png')` }}
+            style={{ backgroundImage: `url(/grass-flower-1.png)` }}
           />
 
           {/**** Scroll arrow */}
